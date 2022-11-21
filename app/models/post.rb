e587feeec02e673730f463d63b88efb5378334df
Felix <barosiofelix@gmail.com>
2022-11-21 16:26:34 +0300

    Completed Assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+    # validates :content, length: { in: 100..1000 }
+    validates :content, length: { minimum: 100 }
+
 end
